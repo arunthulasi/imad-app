dd4f21d8c3889c4c273baebfd44c55febc6da2fd
arunthulasi <nandhanto@gmail.com>
2018-02-16 17:16:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-app.get('/ui/cse.html',function (req,res){
-    res.sendFile(path.join(__dirname,'ui','cse.html'));
+app.get('/ui/cse.html', function (req,res){
+    res.sendFile(path.join(__dirname, 'ui', 'cse.html'));
 });
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
